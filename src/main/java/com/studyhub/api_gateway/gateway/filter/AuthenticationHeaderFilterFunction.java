47417c644549d09af0319546fb92ea4bb884293d
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-06-23 11:39:51 +0900

    feat #1 : preRequest (header에 userId 추가)

---
+++ src/main/java/com/studyhub/api_gateway/gateway/filter/AuthenticationHeaderFilterFunction.java
@@ -0,0 +1,26 @@
+package com.studyhub.api_gateway.gateway.filter;
+
+import com.sun.security.auth.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.function.ServerRequest;
+
+import java.util.function.Function;
+
+@Slf4j
+public class AuthenticationHeaderFilterFunction {
+    public static Function<ServerRequest, ServerRequest> addHeader() {
+        return request -> {
+            ServerRequest.Builder requestBuilder = ServerRequest.from(request);
+
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal instanceof UserPrincipal userPrincipal) {
+                requestBuilder.header("X-Auth-UserId", userPrincipal.getName());
+
+                // 필요 시, 사용자 권한 정보 추가
+            }
+
+            return requestBuilder.build();
+        };
+    }
+}
